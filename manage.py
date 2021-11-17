5703fecac497865f708ed119e10328fefac82eda
amarcel <amarcel.pro@gmail.com>
2021-11-17 04:07:58 +0100

    correct name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sidsales.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seedrill.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- seedrill/wsgi.py
+++ seedrill/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sidsales project.
+WSGI config for seedrill project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sidsales.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seedrill.settings')
 
 application = get_wsgi_application()
