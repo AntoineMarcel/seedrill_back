6c0caf9447f0474ee80eb340cab118f570b7cdcc
amarcel <amarcel.pro@gmail.com>
2021-11-19 03:19:19 +0100

    disable payment

--- integration/urls.py
+++ integration/urls.py
@@ -5,5 +5,5 @@ from .views import Banner_API, Payment_API
 
 urlpatterns = [
     path('banner/<str:sequenceID>/<str:friendCode>', Banner_API.as_view()),
-    path('payment/<str:sequenceID>', Payment_API.as_view())
+    # path('payment/<str:sequenceID>', Payment_API.as_view())
 ]
