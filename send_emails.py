e6155c34ab01a96da9e09afb8b882bfa0c999702
amarcel <amarcel.pro@gmail.com>
2021-11-19 19:20:34 +0100

    change

--- send_emails.py
+++ send_emails.py
@@ -1,4 +1,3 @@
 import requests
 import os
-print(os.getenv("ADMINTOKEN"))
-r = requests.get("http://amarcel.pythonanywhere.com/send_email", headers={"Authorization" : os.getenv("ADMINTOKEN")})
+r = requests.get("http://amarcel.pythonanywhere.com/send_email", headers={"Authorization" :'Token 49163951de59fcb077795bafd34d5d9e57060742'})
